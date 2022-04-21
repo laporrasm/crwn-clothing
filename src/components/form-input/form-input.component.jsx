a3c8402317c620bf8059a7ef0ee237aa4698f49b
Leonardo <lporrasm@gmail.com>
2022-04-21 12:46:27 -0500

    Created sign up component, the form input component, button component

---
+++ src/components/form-input/form-input.component.jsx
@@ -0,0 +1,21 @@
+import './form-input.styles.scss';
+
+/* true && expression always evaluates to expression, and false && expression always evaluates to false */
+const FormInput = ({ label, ...otherProps }) => {
+  return (
+    <div className='group'>
+      <input className='form-input' {...otherProps} />
+      {label && (
+        <label
+          className={`${
+            otherProps.value.length ? 'shrink' : ''
+          } form-input-label`}
+        >
+          {label}
+        </label>
+      )}
+    </div>
+  );
+};
+
+export default FormInput;
